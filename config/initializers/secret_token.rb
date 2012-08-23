3ea12d314e1e9a662b5c1b261c5b3b455c51a93d
Lucas J. Merencia <lucas.merencia@gmail.com>
2012-08-23 16:31:31 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Selftaught::Application.config.secret_token = '61d5f02cfffb3c72e2343c18c4718e072249ef3ace75eccfd2845fc793adfa4c552a2e4a275f9d6fc700ff531c507ea8fd79792de0f371c3929664c8e25d97b5'
